8131bb261b80aac3474ea3e396dc792a1d0c7ba2
hxddad <haddad.yazan117@gmail.com>
2024-04-12 19:33:58 -0400

    update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,20 +17,29 @@ function About() {
         <div className="BigCard">
         <ScrollAnimation animateIn="fadeIn">
           <Image
-            src="/man-svgrepo-com.svg"
+            src="/favicon.ico"
             alt="man-svgrepo"
           />
         </ScrollAnimation>
           <div className="AboutBio">
             <ScrollAnimation animateIn="fadeIn">
-            Hello! My name is <strong>Yazan Haddad</strong>. I'm originally from Brazil and have been living in Canada for the past three years. During my time here, I gained valuable experience through various entry-level positions, which greatly enhanced my work ethic, communication skills, and adaptability.
+            Hello! My name is <strong>Yazan</strong> and I'm currently studying 
+            Software Engineering at York University. In my high school courses, I was 
+            interested in my mathematics, chemistry and physics classes that
+            involved problem-solving, which inspired me to pursue engineering. 
+            I also had a burning interest in technology and the 
+            devices we use today and as a child, that determined the engineering
+            discipline I wanted to get into. 
             </ScrollAnimation>
 
             <br /><br />
             
             <ScrollAnimation animateIn="fadeIn">
-            My journey into the world of technology began with a strong passion for problem-solving, which led me to pursue the Computer Systems Technology program. Despite its challenges, I remained determined and forged lasting friendships that supported me throughout this demanding program.
-            Throughout my studies at BCIT, I had the opportunity to engage in fascinating projects. Notably, I contributed to the development of the RENR Safety App and MyMind, projects that garnered recognition from my colleagues, instructors, and clients alike.
+            From opening a computer for the very first time as a child to our favorite video game
+            in our teen years, we have made irreplaceable memories that shape who we are today. 
+            I want to be able to create these memories and nostalgia that will inspire
+            the next generation to take the reins of our world. I'm grateful that I have
+            the opportunity today to deliver this. 
             </ScrollAnimation>
 
             <br /><br />
@@ -38,14 +47,14 @@ function About() {
             <ScrollAnimation animateIn="fadeIn">
               Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and cross-platform mobile development.
               <div className="tagline2">
-                I have become confident using the following technologies:
+                I'm familiar following technologies:
               </div>
             </ScrollAnimation>
             
 
             <Technologies>
               {stackList.map((stack, index) => (
-                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                <ScrollAnimation animateIn="fadeIn" key={index}>
                   <Tech key={index} className="tech">
                     <TechImg src={stack.img} alt={stack.name} />
                     <TechName>{stack.name}</TechName>
